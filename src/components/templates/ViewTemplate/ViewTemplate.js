11a9d719a2f92c5dbd052d97ae06442ffd34fd05
Artur Chmielewski <arturchmiel96@gmail.com>
2022-03-09 15:51:44 +0100

    task-7 complete

--- src/components/templates/ViewTemplate/ViewTemplate.js
+++ src/components/templates/ViewTemplate/ViewTemplate.js
@@ -1,8 +1,8 @@
 import styled from 'styled-components';
 
 export const ViewTemplate = styled.section`
-    position: relative;
-    display: flex;
-    width: 100%;
-    padding: 0 3rem;
+  position: relative;
+  display: flex;
+  width: 100%;
+  padding: 0 3rem;
 `;
--- src/layout/layout.style.js
+++ src/layout/layout.style.js
@@ -2,4 +2,5 @@ import styled from 'styled-components';
 
 export const Content = styled.div`
   width: 100vw;
+  overflow-x: hidden;
 `;
